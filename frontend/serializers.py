5cd834d49fad466baa44feee2798e2864bdb5a91
frenx <robertruhiu@gmail.com>
2021-07-21 10:37:07 +0300

    swrializer frontend add profile photo

--- frontend/serializers.py
+++ frontend/serializers.py
@@ -39,7 +39,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['id','user','user_type','stage','csa','gender','linkedin_url','github_repo','years','about','skills','verified_skills', 'country','availabilty',
-                  'company','job_role','industry','company_url','file','salary','available','student','notifications','remote_entry','remote_verified','phone_number','design_portfolio']
+                  'company','job_role','industry','company_url','file','salary','available','student','notifications','remote_entry','remote_verified','phone_number','design_portfolio','profile_photo']
 
 class ProfileSerializerUpdater(serializers.ModelSerializer):
     user = UserSerializer
@@ -50,7 +50,7 @@ class ProfileSerializerUpdater(serializers.ModelSerializer):
         fields = ('id', 'user', 'user_type', 'stage', 'csa', 'gender', 'linkedin_url', 'github_repo',
                   'years', 'about', 'skills', 'verified_skills',
                   'country', 'availabilty', 'company', 'job_role', 'industry', 'company_url', 'file', 'salary',
-                  'available', 'student', 'notifications','remote_entry','remote_verified','phone_number','design_portfolio')
+                  'available', 'student', 'notifications','remote_entry','remote_verified','phone_number','design_portfolio','profile_photo')
 
 
 class ReferralSerializer(serializers.ModelSerializer):
